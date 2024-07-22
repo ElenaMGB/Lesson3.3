d00628d8145d0f172318973ccd3bfec7d678f9fd
ElenaMGB <161601927+ElenaMGB@users.noreply.github.com>
2024-07-22 07:40:37 +0500

    fix bugs

--- main.py
+++ main.py
@@ -27,16 +27,13 @@ running = True
 while running:
     screen.fill(color)
     for event in pygame.event.get():
-        if event.type == pygame.quit():
+        if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_height:
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
-                # target_x = mouse_x
-
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
