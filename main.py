9d7e968e320667b9cb9301d7f4a5a38116f84ddd
ElenaMGB <161601927+ElenaMGB@users.noreply.github.com>
2024-07-22 07:31:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,35 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon=pygame.image.load("img/civ740.png")
-pygame.display.set_icon(icon) #Установим изображение как иконку для игры
+icon = pygame.image.load("img/imgRPG.png")
+# Установим изображение как иконку для игры
+pygame.display.set_icon(icon)
 
-target_img=pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png")
 target_width = 80
-target_heigth = 80
+target_height = 80
 
-target_x=random.randint(0, SCREEN_WIDTH-target_width)
-target_y=random.randint(0, SCREEN_HEIGHT-target_heigth)
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-
-running = True #игровой цикл
+# игровой цикл
+running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+                # target_x = mouse_x
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
